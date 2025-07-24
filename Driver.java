2bc714d0ecb22ce564e731286ab4022e8e65ce39
Ryan-B-Runo <43120875+Ryan-B-Runo@users.noreply.github.com>
2025-07-24 09:38:20 -0500

    Make program
    Still have to output the file

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Driver {
 
@@ -18,14 +18,28 @@ public class Driver {
             System.out.println("File is not readable");
         }else{
             Scanner fileScan = new Scanner(file);
-            while(fileScan.hasNextLine()) {
-                data.append(fileScan.nextLine()).append("\n");
+            fileScan.useDelimiter("[^a-zA-Z0-9-]+");
+            int position = 0;
+
+            TreeMap<String, ArrayList<Integer>> wordLocations = new TreeMap<>();
+            HashSet<String> skipWords = new HashSet<>(Arrays.asList("a", "an", "and", "the", "they", "to", "for", "their", "you", "your", "are", "as", "but", "by", "can", "did", "from", "her", "him", "in", "is", "it", "or", "that", "this", "with"));
+
+            while(fileScan.hasNext()) {
+                String word = fileScan.next().toLowerCase();
+
+                if(!skipWords.contains(word)) {
+                    wordLocations.computeIfAbsent(word, k -> new ArrayList<>()).add(position);
+                }
+
+                position++;
+
             }
             fileScan.close();
+
+            for(String word : wordLocations.keySet()) {
+                System.out.println(word + ": " + wordLocations.get(word));
+            }
+
         }
-
-        System.out.println(data);
-
     }
-
 }
